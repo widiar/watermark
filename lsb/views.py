c0b0e1e495e4f9a422e968a7ee7956a8690045a7
widiar <awidiarsana.aw@gmail.com>
2021-10-02 16:05:58 +0800

    'tesseract'

--- lsb/views.py
+++ lsb/views.py
@@ -34,7 +34,7 @@ class HalamanDepanView(View):
             namefile = "dummy" + str(time()) + ".png"
             file_path = os.path.join(media_dir, namefile)
 
-            #cek valid ktp
+            #cek valid ktp using pytesseract
             img = np.array(Image.open(file.file.path))
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             th, threshed = cv2.threshold(gray, 127, 255, cv2.THRESH_TRUNC)
