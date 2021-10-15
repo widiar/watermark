f49be875cdc3a7cd6c010765a8e954fba726f302
widiar <awidiarsana.aw@gmail.com>
2021-10-15 09:48:02 +0800

    fixing

--- lsb/forms.py
+++ lsb/forms.py
@@ -5,5 +5,10 @@ class ImageForm(forms.ModelForm):
     class Meta:
         model = ImageDummy
         fields = '__all__'
+        error_messages = {
+            'name': {
+                'max_length': "Teks Watermark tidak boleh lebih dari 100 karakter.",
+            },
+        }
     
     visible = forms.IntegerField()
